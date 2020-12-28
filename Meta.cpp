ce84334b72c20f75f3c9ea0c09396d383e03130b
diegofariasc <diegofariascastro@MacBook-Air-de-Diego.local>
2020-12-27 18:21:55 -0600

    Added table name and field names to metadata

--- Meta.cpp
+++ Meta.cpp
@@ -3,19 +3,26 @@
 // ------------------------------------------------------------
 // CONSTRUCTORS 
 // ------------------------------------------------------------
-Meta::Meta( unsigned short tupleFieldCount,
-            unsigned short* tupleFieldTypes, 
-            unsigned int* tupleFieldSizes, 
-            unsigned short primaryKeyFieldCount,
-            unsigned short* primaryKeyFields,
-            char** fieldNames )
+Meta::Meta( unsigned short      tableNameLength,
+            char*               tableName,
+            unsigned short      tupleFieldCount,
+            unsigned short*     tupleFieldTypes, 
+            unsigned int*       tupleFieldSizes, 
+            unsigned short      primaryKeyFieldCount,
+            unsigned short*     primaryKeyFields,
+            unsigned short*     fieldNameSizes, 
+            char**              fieldNames  )
 {
     // Set attributes that are given
+    this->tableNameLength = tableNameLength;
+    this->tableName = tableName;
     this->tupleFieldCount = tupleFieldCount;
     this->tupleFieldTypes = tupleFieldTypes;
     this->tupleFieldSizes = tupleFieldSizes;
     this->primaryKeyFieldCount = primaryKeyFieldCount;
     this->primaryKeyFields = primaryKeyFields;
+    this->fieldNames = fieldNames;
+    this->fieldNameSizes = fieldNameSizes;
     this->fieldNames = fieldNames;
 
     // Initialize byte positions array and byte size
@@ -41,14 +48,19 @@ Meta::Meta( unsigned short tupleFieldCount,
 // ------------------------------------------------------------
 
 // Without parameters
+unsigned short  Meta::getTableNameLength()      { return tableNameLength; }
+char*           Meta::getTableName()            { return tableName; }
 unsigned int    Meta::getTupleByteSize()        { return tupleByteSize; }
 unsigned short  Meta::getFieldCount()           { return tupleFieldCount; }
 unsigned short  Meta::getPrimaryFieldCount()    { return primaryKeyFieldCount; }
 
+
 // With parameters
-unsigned short  Meta::getFieldType( unsigned short position )   { return tupleFieldTypes[ position ]; }
-unsigned short  Meta::getFieldSize( unsigned short position )   { return tupleFieldSizes[ position ]; }
-unsigned short  Meta::getFieldStart( unsigned short position )  { return tupleFieldPositions[ position ]; }
+unsigned short  Meta::getFieldType( unsigned short position )       { return tupleFieldTypes[ position ]; }
+unsigned short  Meta::getFieldSize( unsigned short position )       { return tupleFieldSizes[ position ]; }
+unsigned short  Meta::getFieldStart( unsigned short position )      { return tupleFieldPositions[ position ]; }
+unsigned short  Meta::getFieldNameSize( unsigned short position )   { return fieldNameSizes[ position ]; }
+char*           Meta::getFieldName( unsigned short position )       { return fieldNames[ position ]; }
 
 // ------------------------------------------------------------
 // MANDATORY METHOD [VIRTUAL] 
@@ -57,7 +69,8 @@ unsigned short  Meta::getFieldStart( unsigned short position )  { return tupleFi
 // For information about this method refer to Serializable.h
 unsigned int Meta::getSerialFormSize()
 {
-    return  sizeof( tupleByteSize ) + sizeof( tupleFieldCount ) +
+    return  
+            sizeof( tupleByteSize ) + sizeof( tupleFieldCount ) +
             sizeof( unsigned short ) * tupleFieldCount +
             sizeof( unsigned int ) * tupleFieldCount * 2 + 
             sizeof( primaryKeyFieldCount ) + 
--- Meta.h
+++ Meta.h
@@ -17,6 +17,8 @@ class Meta : Serializable
     // ------------------------------------------------------------
     private:
 
+        unsigned short  tableNameLength;            // Length of the table name
+        char*           tableName;                  // Name of the table
         unsigned int    tupleByteSize;              // Total size of the tuples in bytes
         unsigned short  tupleFieldCount;            // Total number of fields
         unsigned short* tupleFieldTypes;            // Array with the types of each field
@@ -24,7 +26,7 @@ class Meta : Serializable
         unsigned int*   tupleFieldPositions;        // Positions at which each field starts
         unsigned short  primaryKeyFieldCount;       // Number of fields in the primary key
         unsigned short* primaryKeyFields;           // Array with the number of fields at which the primary key is found
-        unsigned int*   fieldNameSizes;             // Sizes of each field in bytes
+        unsigned short* fieldNameSizes;             // Sizes of each field in bytes
         char**          fieldNames;                 // Names of the fields
 
 
@@ -35,15 +37,20 @@ class Meta : Serializable
 
         // Constructor methods
         Meta();                                 
-        Meta( unsigned short tupleFieldCount, unsigned short* tupleFieldTypes, 
-              unsigned int* tupleFieldSizes, unsigned short primaryKeyFieldCount, 
-              unsigned short* primaryKeyFields, char** fieldNames );
+        Meta(   unsigned short tableNameLength, char* tableName, unsigned short tupleFieldCount, 
+                unsigned short* tupleFieldTypes, unsigned int* tupleFieldSizes, 
+                unsigned short primaryKeyFieldCount, unsigned short* primaryKeyFields, 
+                unsigned short* fieldNameSizes, char** fieldNames );
 
+        unsigned short  getTableNameLength();                       // Get table name length
+        char*           getTableName();                             // Get the name of the table
         unsigned int    getTupleByteSize();                         // Recover the size of the tuples in bytes
         unsigned short  getFieldCount();                            // Get the number of fields in the tuple
         unsigned short  getFieldType( unsigned short position );    // Get the type of the n-th field in the tuple
         unsigned short  getFieldSize( unsigned short position );    // Get the size of the n-th field in the tuple
-        unsigned short  getFieldStart( unsigned short position );   // Get the size of the n-th field in the tuple
+        unsigned short  getFieldStart( unsigned short position );   // Get the starting byte of the n-th field in the tuple
+        unsigned short  getFieldNameSize( unsigned short position );// Get the size of the name of the n-th field in the tuple
+        char*           getFieldName( unsigned short position );    // Get the size of the name of the n-th field in the tuple
         unsigned short  getPrimaryFieldCount();                     // Get the size of the primary key
 
         char*           serialize();                                // Comply with the virtual serialization method
